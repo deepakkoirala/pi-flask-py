8c37e7e7d89bedeb90c177702aa0bbf8e746f8f4
Sumit Shrestha <rhs4shr@gmail.com>
2020-03-30 14:25:33 -0400

    adding exception handling

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, send_file, Response
-import os
-import time
-import pygame
-import pygame.camera
-from pygame.locals import *
 import io
-from PIL import Image
+import os
+import sys
 import threading
 
-#cam setup
+import pygame
+import pygame.camera
+from PIL import Image
+from flask import Flask, render_template
+
+# cam setup
 pygame.init()
 pygame.camera.init()
-cam = pygame.camera.Camera(pygame.camera.list_cameras()[0],(1920,1080))
+cam = pygame.camera.Camera(pygame.camera.list_cameras()[0], (1920, 1080))
 
 app = Flask(__name__)
 lock = threading.Lock()
@@ -21,25 +21,32 @@ lock = threading.Lock()
 def index():
     return render_template('index.html')
 
+
 @app.route('/project')
 def project():
-    uptime = os.popen("uptime").readline()  
+    uptime = os.popen("uptime").readline()
     temp = os.popen("vcgencmd measure_temp").readline()
     return render_template('project.html', temp=temp, uptime=uptime)
 
+
 @app.route('/videoMonitor')
 def videoMonitor():
-    lock.acquire()
-    cam.start()    
-    img = cam.get_image()
-    cam.stop()
-    lock.release()
-    data = pygame.image.tostring(img, 'RGBA')
-    im = Image.frombytes("RGBA",(1920,1080),data)    
+    try:
+        lock.acquire()
+        cam.start()
+        img = cam.get_image()
+        cam.stop()
+        lock.release()
+        data = pygame.image.tostring(img, 'RGBA')
+        im = Image.frombytes("RGBA", (1920, 1080), data)
+    except:
+        print(sys.exc_info()[0])
+        im = Image.open('/static/images/bg_2.jpg')
     imgByteArr = io.BytesIO()
     im.save(imgByteArr, format='PNG')
-    w=imgByteArr.getvalue()
-    return w, 200, {'content-type':'image/png; charset=utf-8;'}
+    w = imgByteArr.getvalue()
+    return w, 200, {'content-type': 'image/png; charset=utf-8;'}
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
